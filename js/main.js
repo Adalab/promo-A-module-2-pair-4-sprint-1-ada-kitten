56e328c307962cb6b88666b2114a11f4a3c71e5c
Sara Sanchez Crespo <saritasc89@gmail.com>
2023-12-05 19:59:32 +0000

    add Objects and Arrays

--- js/main.js
+++ js/main.js
@@ -8,27 +8,31 @@ const inputName = document.querySelector(".js-input-name");
 const inputBreed = document.querySelector(".js-input-breed");
 const search = document.querySelector(".js_search");
 
-// Variables
+// Objetos kitten
 
-const kittenPhoto1 = "https://dev.adalab.es/gato-siames.webp";
-const kittenPhoto2 = "https://dev.adalab.es/sphynx-gato.webp";
-const kittenPhoto3 = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenData_1 = {
+  image: "https://dev.adalab.es/gato-siames.webp",
+  name: "Anastacio",
+  desc: " Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.",
+  breed: "Siamés",
+};
 
-const kittenName1 = "Anastacio";
-const kittenName2 = "Fiona";
-const kittenName3 = "Cielo";
+const kittenData_2 = {
+  image: "https://dev.adalab.es/sphynx-gato.webp",
+  name: "Fiona",
+  desc: "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.",
+  breed: "Sphynx",
+};
 
-const kittenBreed1 = "Siamés";
-const kittenBreed2 = "Sphynx";
-const kittenBreed3 = "Maine Coon";
+const kittenData_3 = {
+  image: "https://dev.adalab.es/maine-coon-cat.webp",
+  name: "Cielo",
+  desc: "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.",
+  breed: "Maine Coon",
+};
 
-const kittenDesc1 =
-  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
-const kittenDesc2 =
-  "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
-const kittenDesc3 =
-  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
-
+const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
+console.log(kittenDataList[0].name);
 
 // Eventos
 
@@ -75,48 +79,48 @@ function addNewKitten(event) {
 }
 btnAdd.addEventListener("click", addNewKitten);
 
-
 // EVENTOS
 
-function renderKitten(url, desc, name, breed) {
-   return `<li class="card ">
-          <img class="card_img" src= ${url} alt="cat-photo" />
-          <h3 class="card_title">${name}</h3>
-          <h4 class="card_race">${breed}</h4>
+function renderKitten(kittenData) {
+  return `<li class="card ">
+          <img class="card_img" src= ${kittenData.image} alt="cat-photo" />
+          <h3 class="card_title">${kittenData.name}</h3>
+          <h4 class="card_race">${kittenData.breed}</h4>
           <p class="card_description ">
-            ${desc}
+            ${kittenData.desc}
           </p>
           </li>`;
 }
-
 
 const input_search_desc = document.querySelector(".js_in_search_desc");
 const descrSearchText = input_search_desc.value;
 
 const filterKitten = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-      if (kittenDesc1.includes(descrSearchText)) {
-        list.innerHTML += kitten1;
-      }
-      if (kittenDesc2.includes(descrSearchText)) {
-        list.innerHTML += kitten2;
-      }
-      if (kittenDesc3.includes(descrSearchText)) {
-        list.innerHTML += kitten3;
-      }
+  if (kittenData_1.desc.includes(descrSearchText)) {
+    list.innerHTML += kitten1;
+  }
+  if (kittenData_2.desc.includes(descrSearchText)) {
+    list.innerHTML += kitten2;
+  }
+  if (kittenData_3.desc.includes(descrSearchText)) {
+    list.innerHTML += kitten3;
+  }
 };
 
 // buscar por descripción
 
 search.addEventListener("click", filterKitten);
 
-
-
 // CÓDIGO QUE SE EJECUTA AL CARGAR
 
-const kitten1 = renderKitten (kittenPhoto1, kittenDesc1, kittenName1, kittenBreed1);
-const kitten2 = renderKitten (kittenPhoto2, kittenDesc2, kittenName2, kittenBreed2);
-const kitten3 = renderKitten (kittenPhoto3, kittenDesc3, kittenName3, kittenBreed3);
+const kitten1 = renderKitten(kittenData_1);
 
-list.innerHTML = kitten1 + kitten2 + kitten3
+const kitten2 = renderKitten(kittenData_2);
+
+const kitten3 = renderKitten(kittenData_3);
+ 
+
+list.innerHTML = kitten1 + kitten2 + kitten3;
+
